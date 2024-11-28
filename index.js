1ad337eb1ce5a3f60e3fc2f6c418479fe50884cf
Billy Eskein <billyeskein@gmail.com>
2024-11-28 19:27:14 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Code your solution in this file!
+const companyName = 'Scuber';
+console.log(companyName=='Scuber');//True/ Code your solution in this file!
+let mostProfitableNeighborhood = 'Chelsea'
+console.log(mostProfitableNeighborhood=='Chelsea');
+let companyCeo = 'Susan Smith';
+console.log(companyCeo=='Susan Smith');
